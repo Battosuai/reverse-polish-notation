d190f3966298f390963291193df5dc07adf6dcfd
MD. Asifur Rahman <imdjiban@gmail.com>
2019-10-04 01:58:28 +0600

    Last

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,42 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 import Calculator from "../calculator";
+import "./Main.css";
 
 class Main extends Component {
+  state = {
+    userInput: "",
+    result: "",
+    errors: ""
+  };
 
-    state = {
-        userInput:"",
-        result: "",
-        errors: {}
+  onSubmit = e => {
+    e.preventDefault();
+    this.setState({ errors: "" });
+    var string = this.state.userInput;
+    string = string.replace(/  +/g, " ");
+    if (/[^0-9.+\-/*^()]/g.test(string)) {
+      console.log("true");
     }
 
-    componentDidMount(){
-        console.log("Did MOunt")
+    var result = Calculator(string);
+    if (result.error) {
+      this.setState({ errors: "Invalid Expression" });
+    } else {
+      this.setState({ result: result.value });
     }
+  };
 
-    onSubmit = e => {
-        e.preventDefault();
-        var result = Calculator(this.state.userInput)
-        this.setState({result:result});
-    }
+  onChange = e => {
+    this.setState({ userInput: e.target.value });
+  };
 
-    onChange = e =>{
-        this.setState({userInput:e.target.value});
-    }
-
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.onSubmit.bind(this)}>
-                    <label htmlFor="input">Input</label>
-                    <input type="text" name="input" id="input" onChange={this.onChange}/>
-                    <input type="submit" value="Go" />
-                    <p className="result" name="result">{this.state.result}</p>
-                </form>
-
-            </div>
-        )
-    }
+  render() {
+    const { errors } = this.state;
+    return (
+      <div>
+        <form className="sampleForm" onSubmit={this.onSubmit.bind(this)}>
+          <label htmlFor="input">Input</label>
+          <input type="text" name="input" id="input" onChange={this.onChange} />
+          <input type="submit" value="=" />
+          <input type="text" readOnly value={this.state.result} />
+          {errors && <div>{errors}</div>}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Main;
--- src/calculator.js
+++ src/calculator.js
@@ -1,117 +1,93 @@
-function calculator(string) {
-    var stack = [], arrayOfInput = string.split(' ')
-    if (arrayOfInput.length === 1) {
-        return string;
-    } else {
-        var data, variable1, variable2;
-        arrayOfInput.forEach(v => {
-            switch (v) {
-                case "+":
-                    variable2 = stack.pop();
-                    variable1 = stack.pop();
-                    data = add(variable1, variable2);
-                    stack.push(data);
-                    break;
-                case "-":
-                    variable2 = stack.pop();
-                    variable1 = stack.pop();
-                    data = substract(variable1, variable2);
-                    stack.push(data);
-                    break;
-                case "*":
-                    variable2 = stack.pop();
-                    variable1 = stack.pop();
-                    data = multiplication(variable1, variable2);
-                    stack.push(data);
-                    break;
-                case "/":
-                    variable2 = stack.pop();
-                    variable1 = stack.pop();
-                    data = division(variable1, variable2);
-                    stack.push(data);
-                    break;
-                case "sin":
-                    variable1 = stack.pop();
-                    data = sin(variable1);
-                    stack.push(data);
-                    break;
-                case "cos":
-                    variable1 = stack.pop();
-                    data = cos(variable1);
-                    stack.push(data);
-                    break;
-                case "tan":
-                    variable1 = stack.pop();
-                    data = tan(variable1);
-                    stack.push(data);
-                    break;
-                case "square":
-                    variable1 = stack.pop();
-                    data = square(variable1);
-                    stack.push(data);
-                    break;
-                case "sqrt":
-                    variable1 = stack.pop();
-                    data = sqrt(variable1);
-                    stack.push(data);
-                    break;
-                case "^":
-                    variable2 = stack.pop();
-                    variable1 = stack.pop();
-                    data = power(variable1, variable2);
-                    stack.push(data);
-                    break;
-                default:
-                    stack.push(v);
-                    break;
-            }
-        });
-    }
-    // if (stack.length>1) {
-    // 	console.log("error")
-    // }
+const calculator = string => {
+  var stack = [];
+  var result = {};
+  var arrayOfInput = string.split(" ");
+  if (arrayOfInput.length <= 2) {
+    return { error: true };
+  } else {
+    var data, variable1, variable2;
+    arrayOfInput.forEach(v => {
+      switch (v) {
+        case "+":
+          variable2 = stack.pop();
+          variable1 = stack.pop();
+          data = add(variable1, variable2);
+          if (!data.error) {
+            stack.push(data.value);
+          }
+          break;
+        case "-":
+          variable2 = stack.pop();
+          variable1 = stack.pop();
+          data = substract(variable1, variable2);
+          if (!data.error) {
+            stack.push(data.value);
+          }
+          break;
+        case "*":
+          variable2 = stack.pop();
+          variable1 = stack.pop();
+          data = multiplication(variable1, variable2);
+          if (!data.error) {
+            stack.push(data.value);
+          }
+          break;
+        case "/":
+          variable2 = stack.pop();
+          variable1 = stack.pop();
+          data = division(variable1, variable2);
+          if (!data.error) {
+            stack.push(data.value);
+          }
+          break;
+        case "^":
+          variable2 = stack.pop();
+          variable1 = stack.pop();
+          data = power(variable1, variable2);
+          if (!data.error) {
+            stack.push(data.value);
+          }
+          break;
+        default:
+          stack.push(v);
+          break;
+      }
+    });
+  }
+  if (stack.length > 1) {
+    return { error: true };
+  }
+  result = { error: false, value: stack };
+  return result;
+};
 
-    return stack;
-}
+const add = (var1, var2) => {
+  var value = parseFloat(var1) + parseFloat(var2);
+  return { error: false, value: value };
+};
 
-function add(var1, var2) {
-    return parseFloat(var1) + parseFloat(var2);
-}
+const substract = (var1, var2) => {
+  var value = parseFloat(var1) - parseFloat(var2);
+  return { error: false, value: value };
+};
 
-function substract(var1, var2) {
-    return parseFloat(var1) - parseFloat(var2);
-}
+const multiplication = (var1, var2) => {
+  var value = parseFloat(var1) * parseFloat(var2);
+  return { error: false, value: value };
+};
 
-function multiplication(var1, var2) {
-    return parseFloat(var1) * parseFloat(var2);
-}
+const division = (var1, var2) => {
+  if (var2 === "0") {
+    return { error: true };
+  } else {
+    var value = parseFloat(var1) / parseFloat(var2);
+    return { error: false, value: value };
+  }
+};
 
-function division(var1, var2) {
-    return parseFloat(var1) / parseFloat(var2);
-}
-
-function sin(var1) {
-    return Math.sin(parseFloat(var1));
-}
-
-function cos(var1) {
-    return Math.cos(parseFloat(var1));
-}
-
-function tan(var1) {
-    return Math.tan(parseFloat(var1));
-}
-
-function square(var1) {
-    return parseFloat(var1) * parseFloat(var1);
-}
-
-function sqrt(var1) {
-    return Math.sqrt(parseFloat(var1));
-}
-
-function power(var1, var2) {
-    return Math.pow(parseFloat(var1),parseFloat(var2));
-}
+const power = (var1, var2) => {
+  var value = Math.pow(parseFloat(var1), parseFloat(var2));
+  return { error: false, value: value };
+};
 
 export default calculator;
